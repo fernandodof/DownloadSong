8847908dccb3c983d19cfccf9ba510a2aeb39960
Fernando <Fernando@Alienware-M14x>
2014-11-08 14:13:46 -0300

    Song Downloader for Android V0.002

---
+++ gen/com/example/downloadsong/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.downloadsong;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/ifpb/pdm/downloadsong/MainActivity.java
@@ -0,0 +1,44 @@
+package com.ifpb.pdm.downloadsong;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.example.downloadsong.Player;
+import com.example.downloadsong.R;
+
+
+public class MainActivity extends ActionBarActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		ExecuteDownload executeDownload = new ExecuteDownload(MainActivity.this, "http://10f.b.sscdn.co/f/c/2/1/espectromusic-pharrell-williams-happy-myfreemp3eu-6111b6.mp3");
+		executeDownload.execute(new String[]{"test"});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
